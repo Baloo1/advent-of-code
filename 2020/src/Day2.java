ef6940b529f2a552bd21dbd817693fdec4f2cec1
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-01 19:45:46 +0100

    Add Day2 template

---
+++ 2020/src/Day2.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Day2 {
+    public static HashMap<Integer, Integer> input = new HashMap<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Projects\\AdventOfCode\\Day1Input.txt");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            int nextLine = Integer.parseInt(scanner.nextLine());
+            input.put(nextLine, nextLine);
+        }
+    }
+}
+
