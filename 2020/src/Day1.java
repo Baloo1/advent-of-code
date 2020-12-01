ac80713695e3a1baf5f0df5085e03eaf72c11f38
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-01 14:34:51 +0100

    Day 1 Java solution

---
+++ 2020/src/Day1.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Day1 {
+    public static HashMap<Integer, Integer> input = new HashMap<>();
+    public static int triple = 0;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Projects\\AdventOfCode\\Day1Input.txt");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            int nextLine = Integer.parseInt(scanner.nextLine());
+            input.put(nextLine, nextLine);
+        }
+
+        /*Problem 1*/
+        int problem1 = input.values().stream().filter((v) -> input.containsValue(2020 - v)).findFirst().get();
+        System.out.println("Problem 1: " + (2020 - problem1) * problem1);
+
+        /*Problem 2*/
+        int problem2 = input.values().stream().filter((v) -> findTriple(2020 - v)).findFirst().get();
+        System.out.println("Problem 2: " + problem2 * triple * (2020 - problem2 - triple));
+    }
+
+    public static boolean findTriple(Integer value) {
+        Optional<Integer> optionalTriple = input.values().stream().filter(v -> input.containsKey(value - v)).findFirst();
+        optionalTriple.ifPresent(integerIntegerEntry -> triple = integerIntegerEntry);
+        return optionalTriple.isPresent();
+    }
+}
+
