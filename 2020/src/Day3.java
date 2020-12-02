ae8adc3638e1307c793274a453cccb12f4b072bb
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-02 21:07:50 +0100

    Add Day3

---
+++ 2020/src/Day3.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day3 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Projects\\advent-of-code\\2020\\Day3Input.txt");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            scanner.nextLine();
+
+        }
+    }
+}
+
