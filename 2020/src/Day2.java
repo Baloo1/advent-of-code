72d49443144fb9e496af1558f1f57f6b477fccdf
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-02 07:00:07 +0100

    Add Day2 solution

--- 2020/src/Day2.java
+++ 2020/src/Day2.java
@@ -1,19 +1,56 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day2 {
-    public static HashMap<Integer, Integer> input = new HashMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Projects\\advent-of-code\\2020\\Day2Input.txt");
         Scanner scanner = new Scanner(file);
 
+        int problem1 = 0;
+        int problem2 = 0;
         while (scanner.hasNextLine()) {
-            int nextLine = Integer.parseInt(scanner.nextLine());
-            input.put(nextLine, nextLine);
+            String nextLine = (scanner.nextLine());
+
+            String[] split = nextLine.split(": ");
+            String[] letter = split[0].split(" ");
+            String[] splitNbr = letter[0].split("-");
+            int minNum = Integer.parseInt(splitNbr[0]);
+            int maxNum = Integer.parseInt(splitNbr[1]);
+            String letter1 = letter[1];
+
+            /*Problem 1 */
+            int lastIndex = 0;
+            int count = 0;
+            while (lastIndex != -1) {
+
+                lastIndex = split[1].indexOf(letter1, lastIndex);
+
+                if (lastIndex != -1) {
+                    count++;
+                    lastIndex += letter1.length();
+                }
+            }
+            if (minNum <= count && count <= maxNum) {
+                problem1++;
+            }
+
+            /*Problem 2 */
+            String words = split[1];
+            int nbr = 0;
+            if (words.toCharArray()[minNum - 1] == letter1.toCharArray()[0]) {
+                nbr++;
+            }
+            if (words.toCharArray()[maxNum - 1] == letter1.toCharArray()[0]) {
+                nbr++;
+            }
+            if (nbr == 1) {
+                problem2++;
+            }
         }
+        System.out.println("Problem 1: " + problem1);
+        System.out.println("Problem 2: " + problem2);
     }
 }
 
