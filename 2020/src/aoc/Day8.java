1b0e662c1af66516429278aec22c076bcdf93324
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-11 23:43:47 +0100

    Add Day8

---
+++ 2020/src/aoc/Day8.java
@@ -0,0 +1,73 @@
+package aoc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day8 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("./2020/Day8Input.txt");
+        Scanner scanner = new Scanner(file);
+
+        ArrayList<Pair<String, Integer>> input = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String nextLine = scanner.nextLine();
+            String[] split = nextLine.split(" ");
+            String ins = split[0];
+            int nbr = Integer.parseInt(split[1]);
+            input.add(new Pair<>(ins, nbr));
+        }
+
+        /*Problem 1*/
+        int problem1 = runAssembly(input).right();
+        System.out.println(problem1);
+
+        /*Problem 2*/
+        int problem2 = 0;
+        for (int i = 0; i < input.size(); i++) {
+            String inst = input.get(i).left();
+            int val = input.get(i).right();
+            if (!inst.equals("acc")) {
+                Pair<Integer, Integer> testProgram = new Pair<>(0, 0);
+                /*This check could use some cleanup or generalizing*/
+                if (inst.equals("jmp")) {
+                    input.set(i, new Pair<>("nop", val));
+                    testProgram = runAssembly(input);
+                    input.set(i, new Pair<>("jmp", val));
+                } else if (inst.equals("nop")) {
+                    input.set(i, new Pair<>("jmp", val));
+                    testProgram = runAssembly(input);
+                    input.set(i, new Pair<>("nop", val));
+                }
+                if (testProgram.left() == input.size()) {
+                    problem2 = testProgram.right();
+                    break;
+                }
+            }
+        }
+        System.out.println(problem2);
+    }
+
+    /*Should be reusable*/
+    public static Pair<Integer, Integer> runAssembly(ArrayList<Pair<String, Integer>> instructions) {
+        /*Problem 1*/
+        int pc = 0;
+        int acc = 0;
+        boolean[] previousInstructions = new boolean[instructions.size()];
+        while (pc >= 0 && pc < instructions.size() && !previousInstructions[pc]) {
+            previousInstructions[pc] = true;
+            String inst = instructions.get(pc).left();
+            int val = instructions.get(pc).right();
+            if (inst.equals("acc")) {
+                acc += val;
+                pc += 1;
+            } else if (inst.equals("jmp")) {
+                pc += val;
+            } else {
+                pc += 1;
+            }
+        }
+        return new Pair<>(pc, acc);
+    }
+}
